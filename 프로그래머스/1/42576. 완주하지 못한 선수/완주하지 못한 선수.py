1832dd3e6c124627c9962e9786cf74d40a98c336
Hanhui Hong <hanhui1823@gmail.com>
2026-09-18 12:18:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.19 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,14 @@
 def solution(participant, completion):
-    # participant 참석자
-    # completion 완주자
+    count={}
     
-    answer=''
-    
-    count = {}
     for name in participant:
         count[name]=count.get(name,0)+1
-    
+        
     for name in completion:
         count[name]-=1
+        
+    for name in count:    
+        if count[name]>0:
+            answer = name    
     
-    for name in count:
-        if (count[name]==1):
-            answer=name
-    
-    return answer 
+    return answer
