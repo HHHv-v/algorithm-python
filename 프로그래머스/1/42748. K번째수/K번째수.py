0ffce97cc39b75f36aea20e4dc73f9483413f066
Hanhui Hong <hanhui1823@gmail.com>
2026-09-21 21:59:09 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,8 @@
 def solution(array, commands):
-    answer = []
-    
-    for i,j,k in commands:
-        new_arr=[]
-        new_arr=array[i-1:j]
-        new_arr.sort()
-        answer.append(new_arr[k-1])
-    
+    answer=[]
+    for command in commands:
+        new_array=array[command[0]-1:command[1]]
+        new_array.sort()
+        answer.append(new_array[command[2]-1])
+            
     return answer
